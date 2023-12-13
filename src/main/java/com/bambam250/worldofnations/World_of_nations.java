642caf56bfad619ec6ff42738e7765d868aa4ca9
daniel <danielabarrett25@gmail.com>
2023-12-13 15:10:22 -0600

    setup maven environment

---
+++ src/main/java/com/bambam250/worldofnations/World_of_nations.java
@@ -0,0 +1,17 @@
+package com.bambam250.worldofnations;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class World_of_nations extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
